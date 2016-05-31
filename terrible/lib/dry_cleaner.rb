466d3eaa1abcc9f056cd54d29c84699ad1ae31d4
Sam Morgan <s_morgan@me.com>
2016-05-31 15:48:10 +0100

    further obfuscated terrible code

--- terrible/lib/dry_cleaner.rb
+++ terrible/lib/dry_cleaner.rb
@@ -2,8 +2,7 @@ class DryCleaner
   def pick_up_clothes(output = STDOUT)
     driver_name =  [ "James", "Maria", "Tyrrel" ].sample
     van_driver = phone_van_driver(driver_name)
-    van = hire_van(van_driver)
-    check_van_not_broken(van)
+    van = hire_unbroken_van(van_driver)
     output.puts "Your clothes will be picked up soon!"
   end
 
@@ -13,16 +12,14 @@ class DryCleaner
     VanDriver.new(driver_name)
   end
 
-  def hire_van(van_driver)
+  def hire_unbroken_van(van_driver)
     raise "No driver!" unless van_driver
-    Van.hire(van_driver)
-  end
-
-  def check_van_not_broken(van)
+    van = Van.hire(van_driver)
     if van.wheels.broken?
       raise "Van wheels are broken"
     elsif van.engine.broken?
       raise "Van engine is broken"
     end
+    van
   end
 end
